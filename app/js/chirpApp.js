01f1c81bfb8978f1c24764f4c06b2c22a5608a56
Nikhil Thota <nt92@ufl.edu>
2016-05-14 14:42:10 -0400

    added scope variables/functions and populated chirps

--- app/js/chirpApp.js
+++ app/js/chirpApp.js
@@ -1,4 +1,13 @@
 var app = angular.module('chirpApp', []);
 
+//maincontroller is the put into the html where it modifies the code
 app.controller('mainController', function($scope){
+  $scope.posts = [];
+  $scope.newPost = {created_by: '', text: '', created_at: ''};
+
+  $scope.post = function(){
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {created_by: '', text: '', created_at: ''};
+  }
 });

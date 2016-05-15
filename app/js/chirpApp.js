73b010942d1c2b3a89f20ecf6aa7301bbf61c76e
Nikhil Thota <nt92@ufl.edu>
2016-05-15 13:41:44 -0400

    added express modules

--- app/js/chirpApp.js
+++ app/js/chirpApp.js
@@ -11,3 +11,23 @@ app.controller('mainController', function($scope){
     $scope.newPost = {created_by: '', text: '', created_at: ''};
   }
 });
+
+//authcontroller allows for login and register to use the same controller
+app.controller('authController', function($scope){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  postService.getAll().success(function(data){
+    $scope.posts = data;
+  });
+
+  $scope.login = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+  };
+});

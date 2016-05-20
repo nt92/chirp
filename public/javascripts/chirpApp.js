616d99072097c025c31c505edf56ab428a2d61fb
Nikhil Thota <nt92@ufl.edu>
2016-05-19 22:59:54 -0400

    implemented posting functionality

--- public/javascripts/chirpApp.js
+++ public/javascripts/chirpApp.js
@@ -28,10 +28,23 @@ app.config(function($routeProvider){
     });
 });
 
+app.factory('postService', function($http){
+  var baseUrl = "/api/posts";
+  var factory = {};
+  factory.getAll = function(){
+    return $http.get(baseUrl);
+  };
+  return factory;
+});
+
 //maincontroller is the put into the html where it modifies the code
-app.controller('mainController', function($scope){
+app.controller('mainController', function($scope, postService){
   $scope.posts = [];
   $scope.newPost = {created_by: '', text: '', created_at: ''};
+
+  postService.getAll().success(function(data){
+		$scope.posts = data;
+	});
 
   $scope.post = function(){
     $scope.newPost.created_at = Date.now();

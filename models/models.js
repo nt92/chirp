d7bb9c1a320bfcc4692a83a8a04f7c3d825273a4
Nikhil Thota <nt92@ufl.edu>
2016-05-22 17:23:01 -0400

    fixed bug with objectID comparison and register.html partial

--- models/models.js
+++ models/models.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var postSchema = new mongoose.Schema({
-	created_by: { type: Schema.ObjectId, ref: 'User' },		//should be changed to ObjectId, ref "User"
+	created_by: String,		//should be changed to ObjectId, ref "User"
 	created_at: {type: Date, default: Date.now},
 	text: String
 });
--- public/javascripts/chirpApp.js
+++ public/javascripts/chirpApp.js
@@ -22,7 +22,7 @@ app.config(function($routeProvider){
       controller: 'authController'
     })
     //the signup display
-    .when('/register', {
+    .when('/signup', {
       templateUrl: 'register.html',
       controller: 'authController'
     });
